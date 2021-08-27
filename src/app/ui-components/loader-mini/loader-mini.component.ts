6f4277c1428fc91a31c79e919d7914cc19594f88
Alfred Ntiamoah <ntiamoah376@yahoo.com>
2021-08-27 16:10:27 +0000

    Loader added on search page

--- src/app/ui-components/loader-mini/loader-mini.component.ts
+++ src/app/ui-components/loader-mini/loader-mini.component.ts
@@ -1,13 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loader-mini',
   templateUrl: './loader-mini.component.html',
   styleUrls: ['./loader-mini.component.scss']
 })
-export class LoaderMiniComponent implements OnInit {
+export class LoaderMiniComponent implements OnInit, AfterViewInit {
+  topMargin = 0;
+  constructor() {
+    // const script = document.createElement('script');
+    // script.id = 'loader';
+    // script.src = '../../../assets/lottie-loader/lottie.js';
+    // document.head.appendChild(script);
+  }
+  ngAfterViewInit(): void {
+    var animation = (window as any).lottie.loadAnimation({
+      container: document.getElementById('bm'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: '../../../assets/lottie-loader/data.json'
+    })
 
-  constructor() { }
+    // console.log(animation);
+
+  }
+
+
+  @Input() set TopMargin(m: number) {
+    this.topMargin = m;
+  }
 
   ngOnInit(): void {
   }
